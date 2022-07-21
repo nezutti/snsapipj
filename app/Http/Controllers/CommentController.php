9f3e57c4812f8ea3b53697a330870839a9220509
久保祐輔 <bokukuboyu@icloud.com>
2022-07-21 15:14:28 +0900

    CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -73,9 +73,10 @@ class CommentController extends Controller
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Comment $comment)
-    {
-        $item=Comment::where("id",$comment->id)->delete();
+    public function destroy(Request $request)
+     
+    {   $item2=['user_uid'=>$request->user_uid,'message_id'=>$request->message_id];
+        $item=Comment::where($item2)->delete();
          if ($item) {
             return response()->json([
              'message' => 'Deleted successfully',
